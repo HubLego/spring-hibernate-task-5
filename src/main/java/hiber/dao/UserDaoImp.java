52b8990e9510882ba601471211de527e996bec78
Oleg Dmitrochenko <legolost@mail.ru>
2025-07-03 10:49:36 +0300

    Complete task.

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -26,4 +27,17 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCar(String model, int series) {
+      String hql = "FROM User WHERE car.model = :model AND car.series = :series";
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      try {
+         return query.setMaxResults(1).getSingleResult();
+      } catch (NoResultException e) {
+         return null;
+      }
+
+   }
 }
